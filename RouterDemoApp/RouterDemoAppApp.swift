f3ad6cb67ca76868c8f4fdb7a40106d0cd88674e
Kaushal Bhalara <kaushalpatell1989@gmail.com>
2026-01-05 11:58:03 +0530

    Initial Commit

---
+++ RouterDemoApp/RouterDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterDemoAppApp.swift
+//  RouterDemoApp
+//
+//  Created by Kaushal Bhalara on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RouterDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
